4529cac17ab990eccc957b544dadc834893d44e5
flyingsparx <willwebberley@gmail.com>
2015-04-29 18:26:09 +0100

    initial

---
+++ README.md
@@ -0,0 +1,126 @@
+# WekaGo 1.0
+
+A simple Go wrapperaround the Weka Java command-line interface supporting basic classification tasks.
+
+The library abstracts the use of ARFF files and allows users to integrate some of Weka's features programmatically and directly within a Go project.
+
+## Installation
+
+To install WekaGo, retrieve it with:
+
+```bash
+$ go get github.com/flyingsparx/wekago
+```
+
+and then import it into your project:
+
+```go
+import "github.com/flyingsparx/wekago"
+```
+
+## Dependencies
+
+No further Go libraries are required, but you will need working Go and Java environments.
+
+You will also need the Weka `jar` file, downloadable from [their website](http://www.cs.waikato.ac.nz/ml/weka). Currently WekaGo is configured to look for `weka.jar` in the root of your Go project, and will fail if it cannot find it.
+
+## Using
+
+### Getting started
+Most of the functionality provided by WekaGo is achieved through functions exposed by its API and centred around a Weka model. Models can be created using the `NewModel()` function, to which you should pass your desired classifier:
+
+```go
+model := wekago.NewModel("bayes.BayesNet")
+```
+
+
+### Model training
+To train a model, `model` needs to be supplied with a series of feature instances. Individual features can be created by identifying a feature name, value, and the ARFF representation of the feature data type (e.g. `'real'` `'{true, false}`, etc.):
+
+```go
+feature1 := wekago.NewFeature("rain", "true", "{true, false}")
+feature2 := wekago.NewFeature("grass, "wet", "{wet, dry}")
+
+...
+```
+
+To identify features that belong together, they need to be added to an instance:
+
+```go
+instance1 := wekago.NewInstance()
+instance1.AddFeature(feature1)
+instance1.AddFeature(feature2)
+
+instance2 := wekago.NewFeature()
+...
+```
+
+Once you have a series of training feature instances, they can be added directly to the model:
+
+```go
+model.AddTrainingInstance(instance1)
+model.AddTrainingInstance(instance2)
+
+...
+```
+
+Then training the model is simple:
+```go
+err := model.Train()
+```
+
+Any errors will be reported inside `err`, which will be `nil` if all is OK.
+
+Alternatively, if you already have a model you have built before then you can load that:
+
+```go
+model.LoadModel("/path/to/model")
+```
+
+
+### Model testing
+Adding test features is almost exactly the same as adding training features, except, if the value of the outcome feature (the last one added to an instance) is unknown, then replace its value with a `"?"`. Then use `model`'s `AddTestingInstance()` method to add instances of such feature sets:
+
+```go
+test_feature1 := wekago.NewFeature("rain", "true", "{true, false}")
+test_feature2 := wekago.NewFeature("grass", "?", "{wet, dry}")
+
+...
+
+test_instance1 := wekago.NewInstance()
+test_instance1.AddFeature(test_feature1)
+test_instance1.AddFeature(test_feature2)
+
+...
+
+model.AddTestingInstance(test_instance1)
+
+...
+```
+
+Finally, the feature instances can be classified by calling `Test()`:
+
+```go
+err := model.Test()
+```
+
+As before, any errors will be included in `err`.
+
+Testing the model populates its `Predictions` array.
+
+
+
+### Examining classification output
+Once you've classified your test set, you can examine the predictions made by your chosen classifier through your `model`'s `Prediction` slice:
+
+```go
+for _, prediction := range model.Predictions{
+    fmt.Printf("%s\n", prediction.Predicted_value)
+}
+```
+
+In addition to `Predicted_value`, there are other fields are available in this struct:
+
+* `Index` - the number of the prediction corresponding to the position of the corresponding input test eature instance
+* `Observed_value` - the observed value of the corresponding test instance (equal to `"?"` where this was unknown)
+* `Predicted_value` - the predicted value of this instance
---
+++ wekago.go
@@ -0,0 +1,174 @@
+package wekago
+
+import (
+        "fmt"
+        "os/exec"
+        "os"
+        "io/ioutil"
+        "strings"
+        "strconv"
+        "errors"
+        "math/rand"
+)
+
+/* 
+    Declare required structs
+*/
+type prediction struct{
+    Index int64
+    Observed_value string
+    Predicted_value string
+    Probability float64
+}
+
+type instance struct{
+    features []feature
+}
+
+type feature struct{
+    name string
+    value string
+    possible_values string
+}
+
+type model struct{
+    classifier string
+    training_file string
+    testing_file string
+    model_file string
+    training_instances []instance
+    testing_instances []instance
+    model_dir string
+    arff_dir string
+    Predictions []prediction
+    id string
+    trained bool
+}
+
+
+/*
+    Helper functions to initialise new types
+*/
+func NewModel(classifier string) model{
+    model := model{classifier:classifier, id:"test"}
+    model.model_dir = "wekago_data/models"
+    model.arff_dir = "wekago_data/arff"
+    model.id = strconv.Itoa(rand.Int())
+    model.trained = false
+    os.MkdirAll(model.model_dir, 0777)
+    os.MkdirAll(model.arff_dir, 0777)
+
+    return model
+}
+
+func NewFeature(name, value, possible_values string) feature{
+    feature := feature{}
+    feature.name = name
+    feature.value = value
+    feature.possible_values = possible_values
+    return feature
+}
+
+func NewInstance() instance{
+    instance := instance{}
+    return instance
+}
+
+
+/*
+    Methods for model struct
+*/
+func (m *model) AddTrainingInstance(instance instance){
+    m.training_instances = append(m.training_instances, instance)
+}
+
+func (m *model) AddTestingInstance(instance instance){
+    m.testing_instances = append(m.testing_instances, instance)
+}
+
+func (m *model) createARFF(file_type string, instances []instance) (error){
+    file_name := m.arff_dir+"/"+m.id+"_"+file_type+".arff"
+    output := []byte("@relation "+m.id+"\n")
+    for index, instance := range instances{
+        if index == 0{
+            for _, feature := range instance.features{
+                output = append(output, "\t@attribute "+feature.name+" "+feature.possible_values+"\n"...)
+            }
+            output = append(output, "\n@data\n"...)
+        }
+        for index2, feature := range instance.features{
+            if index2 == 0{
+                output = append(output, feature.value...)
+            }else{
+                output = append(output, ","+feature.value...)
+            }
+        }
+        output = append(output," \n"...)
+    }
+    err := ioutil.WriteFile(file_name, output, 0644)
+    if err != nil{
+        return errors.New("Couldn't write ARFF file.")
+    }
+    if file_type == "training"{
+        m.training_file = file_name
+    }
+    if file_type == "testing"{
+        m.testing_file = file_name
+    }
+    return nil
+}
+
+func (m *model) Train() error{
+    write_err := m.createARFF("training", m.training_instances)
+    if write_err != nil{
+        return write_err
+    }
+    save_as := m.model_dir+"/"+m.id+".model"
+    output, java_err := exec.Command("java", "-cp", "./weka.jar", "weka.classifiers."+m.classifier, "-t", m.training_file, "-d", save_as).Output()
+    if java_err != nil || len(output) == 0{
+        return errors.New("Error invoking JVM. Ensure your CLASSPATH is properly set and that your classifier is valid.")
+    }
+    m.trained = true
+    m.model_file = save_as
+    return nil
+}
+
+func (m *model) LoadModel(model_file string) error{
+    m.model_file = model_file
+    m.trained = true
+}
+
+func (m *model) Test() error{
+    if m.model_file == "" || !m.trained{
+        return errors.New("Need to train model first.")
+    }
+
+    m.createARFF("testing", m.testing_instances)
+    output, err := exec.Command("java", "-cp", "./weka.jar", "weka.classifiers."+m.classifier, "-T", m.testing_file, "-l", m.model_file, "-p", "0").Output()
+    if err != nil || len(output) == 0{
+        return errors.New("Error invoking JVM. Ensure your CLASSPATH is properly set and that your classifier is valid.")
+    }
+
+    out_string := string(output[:len(output)])
+    lines := strings.Split(out_string, "\n") 
+    for _, line := range lines{
+        data := strings.Fields(line)
+        if len(data) >= 4 && data[0] != "===" && data[0] != "inst#"{
+            p := prediction{}
+            p.Index, _ = strconv.ParseInt(data[0], 0, 64)
+            p.Observed_value = strings.Split(data[1], ":")[1]
+            p.Predicted_value = strings.Split(data[2], ":")[1]
+            p.Probability, _ = strconv.ParseFloat(data[3], 64)
+            m.Predictions = append(m.Predictions, p)
+        }
+    }
+    return nil;
+}
+
+
+/* 
+    Methods for instance struct
+*/
+func (i *instance) AddFeature(feature feature){
+    i.features = append(i.features, feature)
+}
